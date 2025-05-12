e9941ee4262b9f010a759832b184cc9adea453e1
Joerg Faschingbauer <jf@faschingbauer.co.at>
2025-05-12 10:49:56 +0200

    explain enum class

---
+++ livehacking/enum-class/problem.cpp
@@ -0,0 +1,21 @@
+enum StateMachine1
+{
+    INIT,
+    STATE1,
+    STATE2,
+};
+
+enum StateMachine2
+{
+    INIT,
+    STATE1,
+    STATE2,
+};
+
+int main()
+{
+    StateMachine1 state1 = INIT;
+    StateMachine2 state2 = INIT;
+
+    return 0;
+}
