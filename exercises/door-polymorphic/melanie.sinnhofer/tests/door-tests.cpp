4c02a772f38d3dd412d531d8ce2d24e7c9123046
parallax-district <melanie.sinnhofer@edu.fh-joanneum.at>
2025-06-01 04:17:04 +0200

    push-button done, HUE2 fertig

--- exercises/door-polymorphic/melanie.sinnhofer/tests/door-tests.cpp
+++ exercises/door-polymorphic/melanie.sinnhofer/tests/door-tests.cpp
@@ -3,13 +3,14 @@
 #include <door.h>
 #include <motor-mock.h>
 #include <light-barrier-mock.h>
+#include <push-button-mock.h>
 
 TEST(door_suite, straightforward_open)
 {
     // build a door and its parts
     MotorMock motor(MotorMock::Direction::IDLE);
-    PushButton do_close(PushButton::State::RELEASED);
-    PushButton do_open(PushButton::State::RELEASED);
+    PushButtonMock do_close(PushButtonMock::State::RELEASED);
+    PushButtonMock do_open(PushButtonMock::State::RELEASED);
     LightBarrierMock closed_position(LightBarrierMock::State::BEAM_BROKEN);  // <-- door in "closed" position
     LightBarrierMock opened_position(LightBarrierMock::State::BEAM_SOLID);   // <-- door not in "opened" position
 
