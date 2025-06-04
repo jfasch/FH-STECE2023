8405932b5ce48b008d3140ff24d4d84cab76acea
Majdedin AlRashid <majdedin.alrashid1@edu.fh-joanneum.at>
2025-06-04 11:06:24 +0200

    polymorphic implemented

--- exercises/door-polymorphic/majdedin.alrashid/lib/push-button.h
+++ exercises/door-polymorphic/majdedin.alrashid/lib/push-button.h
@@ -9,13 +9,8 @@ public:
         RELEASED,
     };
 
-public:
-    PushButton(State state);
-    State get_state() const;
-
-    // for tests only
-    void set_state(State state);
-
-private:
-    State _state;
+    virtual ~PushButton() = default;
+    virtual State get_state() const = 0;
+    virtual void set_state(State state) = 0;
+ 
 };
