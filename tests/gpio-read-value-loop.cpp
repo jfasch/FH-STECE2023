3f38f95d30343812b14049dac0c5da2d60358d75
Joerg Faschingbauer <jf@faschingbauer.co.at>
2025-06-05 12:21:56 +0200

    cmake: libgpiod find-module; use C binding

--- tests/gpio-read-value-loop.cpp
+++ tests/gpio-read-value-loop.cpp
@@ -1,23 +1,44 @@
-#include <gpiod.hpp>
+#include <gpiod.h>
 #include <string>
 #include <unistd.h>
+#include <iostream>
+#include <cassert>
 
 
 int main(int argc, char** argv)
 {
     std::string gpiodev = argv[1];
-    int gpionum = std::stoi(argv[2]);
+    unsigned int line = std::stoi(argv[2]);
 
-	auto chip = gpiod::chip(gpiodev);
-    auto request_builder = chip.prepare_request();
-    request_builder.set_consumer("fh-door");
-    request_builder.add_line_settings(
-        gpionum,
-        gpiod::line_settings().set_direction(gpiod::line::direction::INPUT));
-    auto request = request_builder.do_request();
+    auto chip = gpiod_chip_open(gpiodev.c_str());
+	if (!chip) {
+        perror("gpiod_chip_open");
+        return 1;
+    }
+
+	auto settings = gpiod_line_settings_new();
+    assert(settings);
+    
+	gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
+
+	auto line_cfg = gpiod_line_config_new();
+    assert(line_cfg);
+
+    int error = gpiod_line_config_add_line_settings(line_cfg, &line, 1, settings);
+    assert(!error);
+
+    auto request = gpiod_chip_request_lines(chip, nullptr, line_cfg);
+    assert(request != nullptr);
 
     while (true) {
-        std::cout << request.get_value(gpionum) << std::endl;
+        auto value = gpiod_line_request_get_value(request, line);
+        if (value == GPIOD_LINE_VALUE_ACTIVE)
+            std::cout << "active" << std::endl;
+        else if (value == GPIOD_LINE_VALUE_INACTIVE)
+            std::cout << "inactive" << std::endl;
+        else 
+            assert("wtf?");
+
         sleep(1);
     }    
 
