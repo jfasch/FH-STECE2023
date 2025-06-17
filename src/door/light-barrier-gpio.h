90000a18d9d1d5d80c0e9cae91a36a53d1b30d02
finklu <lucas.fink@edu.fh-joanneum.at>
2025-06-17 13:48:44 +0200

    gitigit

--- src/door/light-barrier-gpio.h
+++ src/door/light-barrier-gpio.h
@@ -4,19 +4,20 @@
 #include <string>
 #include <gpiod.hpp>
 
+
 class LightBarrierGPIO : public LightBarrier
 {
     public:
     LightBarrierGPIO(const std::string& gpiodevice, int line_number);
-
+    
     State get_state() const override;
 
 
     private:
     std::string _gpiodevice;
     int _line_number;
-    gpiod::chip chip_;
-    gpiod::line_request _line_request;
+    gpiod::line _line;
+    gpiod::chip _chip;
 
 };  
 
