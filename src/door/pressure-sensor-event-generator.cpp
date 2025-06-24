7d99f818d55f0d50318448ab1eb3deebe083968c
Paul Bichl <paul.bichl@edu.fh-joanneum.at>
2025-06-24 11:38:28 +0200

    updated enum implementations, fixed typo

--- src/door/pressure-sensor-event-generator.cpp
+++ src/door/pressure-sensor-event-generator.cpp
@@ -9,17 +9,17 @@ PressureSensorEventGenerator::PressureSensorEventGenerator(PressureSensor* senso
     this->_under_pressure = under_pressure;
     this->_over_pressure = over_pressure;
 }
-PressuerSensorEvent PressureSensorEventGenerator::get_event() const
+PressureSensorEvent PressureSensorEventGenerator::get_event() const
 {
     float pressure = _sensor->get_pressure();
     
     if (pressure < _under_pressure && pressure >= 0.0f) {
-        return PressuerSensorEvent::UNDER_PRESSURE;
+        return PressureSensorEvent::UNDER_PRESSURE;
     } else if (pressure > _over_pressure) {
-        return PressuerSensorEvent::OVER_PRESSURE;
+        return PressureSensorEvent::OVER_PRESSURE;
     } else if (pressure >= 0.0f) {
-        return PressuerSensorEvent::NORMAL_PRESSURE;
+        return PressureSensorEvent::NORMAL_PRESSURE;
     } else {
-        return PressuerSensorEvent::PRESSURE_SENSOR_ERROR;
+        return PressureSensorEvent::PRESSURE_SENSOR_ERROR;
     }
 }
--- src/door/pressure-sensor-event-generator.h
+++ src/door/pressure-sensor-event-generator.h
@@ -1,21 +1,21 @@
 #pragma once
 #include "pressure-sensor.h"
-#include "pressure-sensor-enums.h"
+
+enum class PressureSensorEvent
+{
+    NORMAL_PRESSURE,
+    UNDER_PRESSURE,
+    OVER_PRESSURE,
+    PRESSURE_SENSOR_ERROR,
+};
 
 class PressureSensorEventGenerator
 {
     public:
-        enum PressureSensorEvent
-        {
-            NORMAL_PRESSURE,
-            UNDER_PRESSURE,
-            OVER_PRESSURE,
-            PRESSURE_SENSOR_ERROR,
-        };
         PressureSensorEventGenerator(PressureSensor* sensor, 
                                      float under_pressure = 0.0f, 
                                      float over_pressure = 0.0f);
-        PressuerSensorEvent get_event() const;
+        PressureSensorEvent get_event() const;
     private:
         PressureSensor* _sensor;
         float _under_pressure;
