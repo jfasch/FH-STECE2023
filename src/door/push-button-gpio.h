d9c94b4b7f84dea6eabcaf866acb50c4dacd7952
Wurzel <wurzel@raspi>
2025-06-24 14:30:56 +0100

    const weg und überhaupt

---
+++ src/door/push-button-gpio.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include "push-button.h"
+#include <gpiod.hpp>
+
+class PushButtonGpio : public PushButton
+{
+public:
+    PushButtonGpio(State state, unsigned int line_offset, const std::string& chipname = "gpiochip0");
+    PushButton::State get_state() override;
+    gpiod::line_request _request;
+
+private:
+ unsigned int line_offset_; 
+  std::string chipname_;
+};
---
+++ tests/push-button-gpio-test.cpp
@@ -0,0 +1,6 @@
+#include <door/push-button-gpio.h>
+#include <std>
+int main(){
+	auto btn = PushButtonGpio(PushButton::RELEASED,1);
+	
+}
