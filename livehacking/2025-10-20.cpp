079b40efeffcaf4fe5525d8fca91168b586cf1a5
Joerg Faschingbauer <jf@faschingbauer.co.at>
2025-11-05 17:11:31 +0100

    (Lec) 2025-11-03

---
+++ livehacking/2025-10-20.cpp
@@ -0,0 +1,22 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <print>
+
+int main(int argc, char** argv)
+{
+    int fd = open("/tmp/somefile", O_WRONLY | O_CREAT | O_EXCL, 0666);
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    
+
+    close(fd);
+
+    return 0;
+}
---
+++ livehacking/uninitialized-memory.cpp
@@ -0,0 +1,13 @@
+#include <print>
+
+int main()
+{
+    int i;
+
+    if (i)
+        std::println("true");
+    else
+        std::println("false");
+
+    return 0;
+}
