b1630953a40ca15912c6c088ce353ddc6c51adf0
LukasSupper <lukas.supper@edu.fh-joanneum.at>
2025-11-10 13:50:41 +0100

    added file structure for little_file_handling

---
+++ src/door/small_file_handling.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include "small_file_handling.h"
+
+void write_string_to_file()
+{
+
+}
+
+void write_numbers_to_file()
+{
+
+}
+
+int read_numbers_from_file()
+{
+    return 0;
+}
+ 
+std::string read_string_from_file()
+{
+    return "";
+}
---
+++ src/door/small_file_handling.h
@@ -0,0 +1,13 @@
+#ifndef SMALL_FILE_HANDLING_H
+#define SMALL_FILE_HANDLING_H
+
+#include <string>
+
+void write_string_to_file();
+
+void write_numbers_to_file();
+
+int read_numbers_from_file();
+ 
+std::string read_string_from_file();
+#endif
