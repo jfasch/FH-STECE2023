202aca318a34b0f263a9610521e6113b5696bbaf
student <student@debian.lab.se.org>
2025-11-10 11:41:30 +0100

    addded analog sensor event generator

---
+++ src/door/analog-sensor-event-generator.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "analog-sensor.h"
+
+enum class AnalogSensorEvent
+{
+    NORMAL_VALUE,
+    UNDER_VALUE,
+    OVER_VALUE,
+    ANALOG_SENSOR_ERROR,
+};
+
+class AnalogSensorEventGenerator
+{
+    public:
+        AnalogSensorEventGenerator(Analog_Sensor* sensor, 
+                                     float under_value = 0.0f, 
+                                     float over_value = 0.0f,
+                                     bool hysteresis_enabled = false,
+                                     float hysteresis_value = 0.01f);
+
+        // setters for hysteresis
+        void enable_hysteresis(bool enable);
+        void set_hysteresis_value(float value);
+
+        // get_event updates internal state when hysteresis is enabled
+        AnalogSensorEvent get_event();
+    private:
+        Analog_Sensor* _sensor;
+        float _under_value;
+        float _over_value;
+        
+        // hysteresis configuration
+        bool _hysteresis_enabled;
+        float _hysteresis_value;
+
+        // state for hysteresis behavior
+        AnalogSensorEvent _last_event;
+        bool _initialized;
+};
